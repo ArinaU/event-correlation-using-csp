db167175abc68d2b27512764c9b7b744aca63083
ul.arina09 <ul.arina09@gmail.com>
2022-08-04 19:57:06 +0200

    try to add other contraints for other cases

--- main.py
+++ main.py
@@ -10,25 +10,50 @@ import itertools
 import math
 
 
+# def generate_condition(*args, **kwargs):
+#     def f():
+#         flag = True
+#         if len(args) == 2:
+#             x = args[0]  # B
+#             y = args[1]  # A
+#             if pd.isna(x) or pd.isna(y):
+#                 return True
+#             else:
+#                 attr = kwargs['attrs'][0]
+#                 attr2 = kwargs['attrs'][1]
+#                 # B == A and A == B
+#                 if (x[attr] == kwargs['vals'][0] and y[attr] == kwargs['vals'][1]) or (
+#                         y[attr] == kwargs['vals'][0] and x[attr] == kwargs['vals'][1]):
+#                     flag = eval(f"{x['EventID']} {kwargs['operator']} {y['EventID']}")
+#         elif len(args) == 1:
+#             x = args[0]
+#             attr = kwargs['attr']
+#             flag = eval(f"x[attr] {kwargs['operator']} {kwargs['val']}")
+#         return flag
+#
+#     return f
+
+
 def generate_condition(*args, **kwargs):
     def f():
         flag = True
+        constraints = kwargs['constraints']
         if len(args) == 2:
             x = args[0]  # B
             y = args[1]  # A
             if pd.isna(x) or pd.isna(y):
                 return True
             else:
-                attr = kwargs['attrs'][0]
-                attr2 = kwargs['attrs'][1]
+                attr = constraints['attrs'][0]
+                attr2 = constraints['attrs'][1]
                 # B == A and A == B
-                if (x[attr] == kwargs['vals'][0] and y[attr] == kwargs['vals'][1]) or (
-                        y[attr] == kwargs['vals'][0] and x[attr] == kwargs['vals'][1]):
-                    flag = eval(f"{x['EventID']} {kwargs['operator']} {y['EventID']}")
+                if (x[attr] == constraints['vals'][0] and y[attr] == constraints['vals'][1]) or (
+                        y[attr] == constraints['vals'][0] and x[attr] == constraints['vals'][1]):
+                    flag = eval(f"{x['EventID']} {constraints['operator']} {y['EventID']}")
         elif len(args) == 1:
             x = args[0]
-            attr = kwargs['attr']
-            flag = eval(f"x[attr] {kwargs['operator']} {kwargs['val']}")
+            attr = constraints['attr']
+            flag = eval(f"x[attr] {constraints['operator']} {constraints['val']}")
         return flag
 
     return f
@@ -97,36 +122,15 @@ def get_matrix1():
                     var = list(solutions[0].values())[0]['EventID']
                     df2 = df2.append({'Case1': solutions[0][var], 'Case2': np.NaN, 'Case3': np.NaN}, ignore_index=True)
                     break
-    #         elif col == 'Case2':
-    #             new_column = curr
 
-    #             solutions = get_solutions(curr, column = new_column, constraints = [unary_constraint2])
-    #             if solutions:
-    #                 var = list(solutions[0].values())[0]['EventID']
-    #                 df2 = df2.append({'Case1': np.NaN, 'Case2': solutions[0][var], 'Case3': np.NaN}, ignore_index=True)
-    #                 break
-    #         elif col == 'Case3':
-    #             new_column = curr
-
-    #             solutions = get_solutions(curr, column = new_column, constraints = [unary_constraint3])
-    #             if solutions:
-    #                 var = list(solutions[0].values())[0]['EventID']
-    #                 df2 = df2.append({'Case1': np.NaN, 'Case2': solutions[0][var], 'Case3': np.NaN}, ignore_index=True)
-    #                 break
     return df2
 
 
 def get_matrix():
     df = pd.read_csv('data1.csv', sep=';')
-
-    # n_of_cases = len(df)
-
     cases = ['Case1', 'Case2', 'Case3']
 
     n_of_events = len(df['EventID'])
-
-    # df2 = pd.DataFrame(columns=["Case1", "Case2", "Case3"])
-
 
 
     dict = { 'Case1': [{'EventID': 1, 'Activity': 'A', 'Timestamp': '2022-01-01 11:01:58', 'UserID': 1},
@@ -134,30 +138,9 @@ def get_matrix():
              'Case2': np.NaN,
              'Case3': np.NaN }
 
-    # dict = [{'EventID': 1, 'Activity': 'A', 'Timestamp': '2022-01-01 11:01:58', 'UserID': 1},
-    #         {'EventID': 2, 'Activity': 'B', 'Timestamp': '2022-01-01 11:10:58', 'UserID': 1}]
-
-    # df2 = pd.DataFrame(dict) #!! in a certain case
-
     df2 = pd.DataFrame(dict)
 
     problem = Problem()
-
-    # for row in range(n_of_events):
-    #     curr = df.loc[row].to_dict()
-    #     # for case in cases:
-    #
-    #     dict = { 'Case1': curr, 'Case2': curr, 'Case3': curr }
-    #     df3 = df2.append(dict, ignore_index=True)
-    #
-    #     data = df3.to_dict()
-    #
-    #     problem.addVariable('Data', data)
-    #     problem.addVariable('Cases', ['Case1', 'Case2', 'Case3'])
-    #
-    #     problem.addConstraint(my_constraint, ['Data', 'Cases'])
-    #     print(problem.getSolutions())
-    #     problem.reset()
 
     curr = df.loc[2].to_dict()
 
@@ -166,20 +149,27 @@ def get_matrix():
 
     data = df3.to_dict()
 
-    # problem.addVariable('Data', data)
     problem.addVariable('Cases', ['Case1', 'Case2', 'Case3'])
 
-    problem.addConstraint(MyConstraint(data), ['Cases'])
+    # constraints = {'attrs': ['Activity', 'Activity'], 'vals': ['A', 'C'], 'operator': ">"}
+
+    constraints = {'Case1': {'attrs': ['Activity', 'Activity'], 'vals': ['A', 'C'], 'operator': ">"},
+                   'Case2': {'attrs': ['Activity', 'UserID'], 'vals': ['A', '2'], 'operator': "<"},
+                   'Case3': {'attr': ['Activity'], 'val': 'B', 'operator': "!="}}
+
+    problem.addConstraint(MyConstraint(data, constraints), ['Cases'])
     print(problem.getSolutions())
 
 
 class MyConstraint(Constraint):
 
-    def __init__(self, data):
+    def __init__(self, data, constraints):
         self._data = data
+        self._constraints = constraints
 
     def __call__(self, variables, domains, assignments, forwardcheck=False):
         data = self._data
+        constraints = self._constraints
         for variable in variables:
             if variable in assignments:
                 case = assignments[variable]
@@ -187,7 +177,7 @@ class MyConstraint(Constraint):
                 for x, y in itertools.combinations(column, 2):
                     x = column[x]
                     y = column[y]
-                    cond = generate_condition(x, y, attrs=['Activity', 'Activity'], vals=['A', 'C'], operator=">")
+                    cond = generate_condition(x, y, constraints = constraints[case])
                     flag = cond()
                     if not flag:
                         return False
@@ -209,19 +199,13 @@ class MyConstraint(Constraint):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # df2 = get_matrix()
-    #
-    # var1 = 'Case1'
-    # var2 = 'Case2'
-    # var3 = 'Case3'
-    #
-    # problem = Problem()
-    #
-    # problem.addVariable('Event', [var1, var2, var3])
-    #
-    # problem.addConstraint(my_constraint, ['Event'])
-    #
-    # print(problem.getSolutions())
+
+    # UserID 3 !=
+    # dict = {'Case1': { 'attrs': ['Activity', 'Activity'], 'vals': ['A', 'C'], 'operator': ">" },
+    #         'Case2': { 'attrs': ['UserID'], 'vals': ['2'], 'operator': "!=" }}
+    # print()
+
+    # cond = generate_condition(x, y, attrs=['Activity', 'Activity'], vals=['A', 'C'], operator=">")
 
     get_matrix()
 
