7ade05e583e8f9282e79c307a84383feb8b134cf
Arina Ulanova <ul.arina09@gmail.com>
2023-04-10 14:52:51 +0200

    refactor Absence

--- constraints/existence_constraints.py
+++ constraints/existence_constraints.py
@@ -5,36 +5,31 @@ from constraints.base_event_constraint import BaseEventConstraint
 
 
 # A occurs at most once
-class Absence(Constraint):
+class Absence(BaseEventConstraint):
 
     def __init__(self, data, required_event, start_event = None):
         self._data = data
         self._required_event = required_event
         self._start_event = start_event
-        self._buf = {}
-
-    def clear_buf(self, curr_event):
-        buf = self._buf.copy()
-        for key, value in buf.items():
-            if value >= curr_event:
-                del self._buf[key]
-
+        self._case_status = {}
 
     def __call__(self, events, domains, assignments, forwardcheck=False):
         data = self._data
-        buf = self._buf
         required_attr = self._required_event['attr']
         required_value = self._required_event['value']
         curr_id = list(assignments)[-1]
+        curr_case = assignments[curr_id]
 
-        self.clear_buf(curr_id)
+        self._case_status = self.clean_struct(assignments, self._case_status)
+
+        if not self._case_status.get(curr_case, None):
+            self._case_status[curr_case] = {}
 
         if data[curr_id][required_attr] == required_value:
-            if buf.get(assignments[curr_id], None):
-                #if buffer[assignments[curr_event_id]]: # if event was before
+            if self._case_status[curr_case]:
                 return False
             else:
-                buf[assignments[curr_id]] = curr_id
+                self._case_status[curr_case] = curr_id
 
         return True
 
