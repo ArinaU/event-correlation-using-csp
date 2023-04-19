8ceddff0d3739dc3b66e2628335f5a615fc0e39d
Arina Ulanova <ul.arina09@gmail.com>
2023-04-19 11:07:11 +0200

    refactor NotChainSuccession

--- constraints/negative_relation_constraints.py
+++ constraints/negative_relation_constraints.py
@@ -77,32 +77,26 @@ class NotSuccession(BaseEventConstraint):
 class NotChainSuccession(BaseEventConstraint):
 
     def __call__(self, events, domains, assignments, forwardcheck=False):
-        data = self._data
-        case_status = self._case_status
-        required_attr = self._required_event['attr']
-        required_value = self._required_event['value']
-        required_attr2 = self._required_event2['attr']
-        required_value2 = self._required_event2['value']
 
-        curr_id = list(assignments)[-1]
-        curr_case = assignments[curr_id]
+        curr_event = list(assignments)[-1]
+        curr_case = assignments[curr_event]
 
-        case_status = self.clean_struct(assignments, case_status)
+        self.case_status = self.clean_struct(assignments, self.case_status)
 
-        if not case_status.get(curr_case, None):
-            case_status[curr_case] = []
+        if not self.case_status.get(curr_case, None):
+            self.case_status[curr_case] = []
 
 
         # if A
-        if data[curr_id][required_attr] == required_value:
-            case_status[curr_case].append({'e': curr_id})
+        if self.data[curr_event][self.attr] == self.val:
+            self.case_status[curr_case].append({'e': curr_event})
         # if C
-        elif data[curr_id][required_attr2] == required_value2:
-            case_events = [e for e, c in assignments.items() if c == curr_case and e < curr_id]
+        elif self.data[curr_event][self.attr2] == self.val2:
+            case_events = [e for e, c in assignments.items() if c == curr_case and e < curr_event]
             if case_events:
                 prev_id = case_events[-1]
                 # if prev event was A
-                if data[prev_id][required_attr] == required_value:
+                if self.data[prev_id][self.attr] == self.val:
                     return False
 
         return True
