bc9156f48ae468d60c525da3f1937bb24a1ccd69
Arina Ulanova <ul.arina09@gmail.com>
2023-04-27 11:56:07 +0200

    try refactor base class

--- constraints/base_event_constraint.py
+++ constraints/base_event_constraint.py
@@ -68,20 +68,21 @@ class BaseEventConstraint(Constraint):
         else:
             attr, val = self.attr, self.val
 
-        empty_cases = {}
+        # empty_cases = {}
         possible_cases = {}
         for case, status in self.case_status.items():
             if case in domains[curr_event]:
                 if not status[event_type] and not status[other_event_type]:
                     continue
-                if not status[other_event_type]:
-                    empty_cases.setdefault(event_type, []).append(case)
+                # if not status[other_event_type]:
+                #     empty_cases.setdefault(event_type, []).append(case)
                 elif len(status[other_event_type]) > len(status[event_type]):
                     possible_cases.setdefault(event_type, []).append(case)
 
-        return empty_cases, possible_cases
+        # return empty_cases, possible_cases
+        return possible_cases
 
-    def check_future_case_assignment(self, events, domains, assignments, empty_cases, event_type):
+    def check_future_case_assignment(self, events, domains, assignments, curr_case, event_type):
         if event_type == 'e':
             attr, val = self.attr2, self.val2
         else:
@@ -89,20 +90,18 @@ class BaseEventConstraint(Constraint):
         all_cases_occur = True
         found_cases = set()
 
-        for case in empty_cases[event_type]:
-            case_occurs = False
-            for future_event in events:
-                if future_event not in assignments:
-                    if self.data[future_event][attr] == val:
-                        if case in domains[future_event] and case not in found_cases:
-                            found_cases.add(case)
-                            case_occurs = True
-                            break
+        # for case in empty_cases[event_type]:
+        case_occurs = False
+        for future_event in events:
+            if future_event not in assignments:
+                if self.data[future_event][attr] == val:
+                    if curr_case in domains[future_event]:
+                        case_occurs = True
+                        break
             if not case_occurs:
-                all_cases_occur = False
                 break
 
-        return found_cases
+        return case_occurs
 
 
     def check_case_status(self, events, domains, assignments, event_type):
@@ -114,31 +113,36 @@ class BaseEventConstraint(Constraint):
         else:
             attr, val = self.attr, self.val
 
-        empty_cases, possible_cases = self.check_possible_cases(events, domains, assignments, event_type)
+        # empty_cases, possible_cases = self.check_possible_cases(events, domains, assignments, event_type)
+        possible_cases = self.check_possible_cases(events, domains, assignments, event_type)
 
-        if not empty_cases:
-            return True
+        # if not empty_cases:
+        #     return True
 
         # Cases with no 'e': ['Case2']
         # Cases with superfluous 'e2': ['Case1']
 
-        found_cases = self.check_future_case_assignment(events, domains, assignments, empty_cases,
+        found_case = self.check_future_case_assignment(events, domains, assignments, curr_case,
                                                                     event_type)
-
-        if set(empty_cases[event_type]) == found_cases:
+        if found_case:
             return True
 
-        remaining_cases_occur = True
-        for event_type, cases in possible_cases.items():
-            remaining_cases = set(empty_cases[event_type]) - found_cases
-            remaining_cases_occur = True
-            for case in remaining_cases:
-                if case not in possible_cases.get(event_type, []):
-                    remaining_cases_occur = False
-                    break
-
-        if remaining_cases_occur:
+        if possible_cases:
             return False
+
+        return True
+
+        # remaining_cases_occur = True
+        # for event_type, cases in possible_cases.items():
+        #     remaining_cases = set(empty_cases[event_type]) - found_cases
+        #     remaining_cases_occur = True
+        #     for case in remaining_cases:
+        #         if case not in possible_cases.get(event_type, []):
+        #             remaining_cases_occur = False
+        #             break
+        #
+        # if remaining_cases_occur:
+        #     return False
 
 
 
--- constraints/mutual_relation_constraints.py
+++ constraints/mutual_relation_constraints.py
@@ -5,39 +5,62 @@ from constraints.base_event_constraint import BaseEventConstraint
 # If B occurs, then A occurs and vice versa: <C, A, C, B, B>, <B, C, C, A>
 class Coexistence(BaseEventConstraint):
 
-    def has_available_cases(self, domains, assignments, event_type):
+    # def has_available_cases(self, domains, assignments, event_type):
+    #     curr_event = list(assignments)[-1]
+    #     curr_case = assignments[curr_event]
+    #     event_domains = domains[curr_event]
+    #     other_event_type = 'e2' if event_type == 'e' else 'e'
+    #
+    #     available_cases = []
+    #
+    #     # if both B and C
+    #     if self.case_status[curr_case][event_type] and self.case_status[curr_case][other_event_type]:
+    #         for case, events in self.case_status.items():
+    #             if case in event_domains:
+    #                 events = self.find_events_in_list(curr_event, case, event_type)  # B
+    #                 other_events = self.find_events_in_list(curr_event, case, other_event_type)  # C
+    #
+    #                 # if empty B and any C
+    #                 if not events and other_events:
+    #                     return [case]
+    #     else: # no B and no C | only B and no C
+    #         for case, events in self.case_status.items():
+    #             if case in event_domains:
+    #                 events = self.find_events_in_list(curr_event, case, event_type)  # B
+    #                 other_events = self.find_events_in_list(curr_event, case, other_event_type)  # C
+    #
+    #                 # if empty B and any C
+    #                 if not events and other_events:
+    #                     return [case]
+    #                 # else:
+    #                 #     # if both B and C
+    #                 #     if events and other_events:
+    #                 #         available_cases.append(case)
+    #     return available_cases
+
+
+    def check_possible_cases(self, events, domains, assignments, event_type):
+        other_event_type = 'e2' if event_type == 'e' else 'e'
         curr_event = list(assignments)[-1]
         curr_case = assignments[curr_event]
-        event_domains = domains[curr_event]
-        other_event_type = 'e2' if event_type == 'e' else 'e'
+        if event_type == 'e':
+            attr, val = self.attr2, self.val2
+        else:
+            attr, val = self.attr, self.val
 
-        available_cases = []
+        # empty_cases = {}
+        possible_cases = {}
+        for case, status in self.case_status.items():
+            if case in domains[curr_event]:
+                if not status[event_type] and not status[other_event_type]:
+                    continue
+                # if not status[other_event_type]:
+                #     empty_cases.setdefault(event_type, []).append(case)
+                elif len(status[other_event_type]) > len(status[event_type]):
+                    possible_cases.setdefault(event_type, []).append(case)
 
-        # if both B and C
-        if self.case_status[curr_case][event_type] and self.case_status[curr_case][other_event_type]:
-            for case, events in self.case_status.items():
-                if case in event_domains:
-                    events = self.find_events_in_list(curr_event, case, event_type)  # B
-                    other_events = self.find_events_in_list(curr_event, case, other_event_type)  # C
-
-                    # if empty B and any C
-                    if not events and other_events:
-                        return [case]
-        else: # no B and no C | only B and no C
-            for case, events in self.case_status.items():
-                if case in event_domains:
-                    events = self.find_events_in_list(curr_event, case, event_type)  # B
-                    other_events = self.find_events_in_list(curr_event, case, other_event_type)  # C
-
-                    # if empty B and any C
-                    if not events and other_events:
-                        return [case]
-                    # else:
-                    #     # if both B and C
-                    #     if events and other_events:
-                    #         available_cases.append(case)
-        return available_cases
-
+        # return empty_cases, possible_cases
+        return possible_cases
 
     def __call__(self, events, domains, assignments, forwardcheck=False):
         curr_event = list(assignments)[-1]
@@ -57,23 +80,20 @@ class Coexistence(BaseEventConstraint):
 
         # if B
         if self.data[curr_event][self.attr] == self.val:
-            if not (self.case_status[curr_case]['e2'] and not self.case_status[curr_case]['e']):
-                if self.check_rejection(domains, assignments, 'e'):
+            if not self.case_status[curr_case]['e2'] \
+                    or (self.case_status[curr_case]['e'] and self.case_status[curr_case]['e2']):
+                if not self.check_case_status(events, domains, assignments, 'e'):
                     return False
 
             self.case_status[curr_case].setdefault('e', []).append(curr_event)
 
-            if not self.check_case_status(events, domains, assignments, 'e'):
-                return False
         elif self.data[curr_event][self.attr2] == self.val2:
-            if not (self.case_status[curr_case]['e'] and not self.case_status[curr_case]['e2']):
-                if self.check_rejection(domains, assignments, 'e2'):
+            if not self.case_status[curr_case]['e'] \
+                    or (self.case_status[curr_case]['e'] and self.case_status[curr_case]['e2']):
+                if not self.check_case_status(events, domains, assignments, 'e2'):
                     return False
 
             self.case_status[curr_case].setdefault('e2', []).append(curr_event)
-
-            if not self.check_case_status(events, domains, assignments, 'e2'):
-                return False
 
             # if B
             # if no B and no C
