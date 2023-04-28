43979360dbeb46e85a59f2dac53aebbe0ffe47d3
Arina Ulanova <ul.arina09@gmail.com>
2023-04-28 12:38:01 +0200

    try Existence

--- constraints/existence_constraints.py
+++ constraints/existence_constraints.py
@@ -30,9 +30,48 @@ class Absence(BaseEventConstraint):
 
 # A occurs at least once
 class Existence(BaseEventConstraint):
-    def reject_conditions(self, event, case, event_type):
-        events = self.find_events_in_list(event, case, event_type)
-        return events
+    # def check_future_case_assignment(self, events, domains, assignments, curr_case, event_type, target_type):
+    #     target_attr, target_val = self.attr2, self.val2
+    #
+    #     # for case in empty_cases[event_type]:
+    #     case_occurs = False
+    #     for future_event in events:
+    #         if future_event not in assignments:
+    #             if self.data[future_event][target_attr] == target_val:
+    #                 if curr_case in domains[future_event]:
+    #                     case_occurs = True
+    #                     break
+    #
+    #     return case_occurs
+
+    # def check_possible_cases(self, events, domains, assignments, event_type, target_type=None):
+    #     # other_event_type = 'e2' if event_type == 'e' else 'e'
+    #     curr_event = list(assignments)[-1]
+    #     curr_case = assignments[curr_event]
+    #
+    #     possible_cases = {}
+    #     for case, status in self.case_status.items():
+    #         if case in domains[curr_event]:
+    #             if not self.case_status[case]['e']:
+    #                 possible_cases.setdefault(event_type, []).append(case)
+    #
+    #     return possible_cases
+
+    def forward_check_events(self, events, domains, assignments):
+        curr_event = list(assignments)[-1]
+        curr_case = assignments[curr_event]
+
+        for event in events:
+            if event not in assignments:
+                if self.data[event][self.attr] == self.val:
+                    domain = domains[event]
+                    if curr_case in domain:
+                        for case in domain[:]:
+                            if case == curr_case:
+                                domain.hideValue(case)
+                        return True
+                    else:
+                        return True
 
     def __call__(self, events, domains, assignments, forwardcheck=False):
         curr_event = list(assignments)[-1]
@@ -48,19 +87,28 @@ class Existence(BaseEventConstraint):
         # A,B,A,B,B,C
         # 1 1 2 2 1 1
 
+        # 1 2 3 4 5 6
+        # A,B,A,B,B,C
+        # 1 1 2 2 1 1
+
+        # 1 2 3 4 5 6 7 8 9
         # A,C,B,A,A,B,C,C,B
         # 1 1 1 2 3 2 1 1 3
+
+        # 1 2 3 4 5
+        # A,A,B,B,C
+        # 1 2 1 2 1
 
         # 1 2 3 4 5 6 7 8 9
         # A,B,B,C,B,C,A,B,C
         # 1 1 1 1 1 1 2 2 1
 
         if self.data[curr_event][self.attr] == self.val:
-            if self.reject_conditions(curr_event, curr_case, 'e'):
-                if self.check_rejection(domains, assignments, 'e'):
-                    return False
 
             self.case_status[curr_case]['e'].append(curr_event)
+
+            if [c for c, e in self.case_status.items() if not e['e']]:
+                self.forward_check_events(events, domains, assignments)
 
         self.prev_assignments[curr_event] = None
 
